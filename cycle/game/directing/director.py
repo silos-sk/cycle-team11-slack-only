0ee45e40cf54178087f719ef77f1ee5b65c74691
deadshadow99 <97075743+deadshadow99@users.noreply.github.com>
2022-06-16 23:06:41 -0500

    Update director.py

--- cycle/game/directing/director.py
+++ cycle/game/directing/director.py
@@ -1,24 +1,27 @@
 class Director:
-    """A person who directs the game. 
-    
+    """A person who directs the game.
+
     The responsibility of a Director is to control the sequence of play.
 
     Attributes:
+    ---
         _video_service (VideoService): For providing video output.
     """
 
     def __init__(self, video_service):
         """Constructs a new Director using the specified video service.
-        
+
         Args:
+        ---
             video_service (VideoService): An instance of VideoService.
         """
         self._video_service = video_service
-        
+
     def start_game(self, cast, script):
         """Starts the game using the given cast and script. Runs the main game loop.
 
         Args:
+        ---
             cast (Cast): The cast of actors.
             script (Script): The script of actions.
         """
@@ -31,12 +34,13 @@ class Director:
 
     def _execute_actions(self, group, cast, script):
         """Calls execute for each action in the given group.
-        
+
         Args:
+        ---
             group (string): The action group name.
             cast (Cast): The cast of actors.
             script (Script): The script of actions.
         """
-        actions = script.get_actions(group)    
+        actions = script.get_actions(group)
         for action in actions:
-            action.execute(cast, script)          
+            action.execute(cast, script)
