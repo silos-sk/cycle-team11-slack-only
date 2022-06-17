976cb44f97bee02db6978773e32579d5bd46de1c
deadshadow99 <97075743+deadshadow99@users.noreply.github.com>
2022-06-16 23:11:27 -0500

    Update keyboard_service.py

--- cycle/game/services/keyboard_service.py
+++ cycle/game/services/keyboard_service.py
@@ -1,20 +1,20 @@
 import pyray
-from game.shared.point import Point
 
 
 class KeyboardService:
-    """Detects player input. 
-    
+    """Detects player input.
+
     The responsibility of a KeyboardService is to indicate whether or not a key is up or down.
 
     Attributes:
+    ---
         _keys (Dict[string, int]): The letter to key mapping.
     """
 
     def __init__(self):
         """Constructs a new KeyboardService."""
         self._keys = {}
-        
+
         self._keys['w'] = pyray.KEY_W
         self._keys['a'] = pyray.KEY_A
         self._keys['s'] = pyray.KEY_S
@@ -27,8 +27,9 @@ class KeyboardService:
 
     def is_key_up(self, key):
         """Checks if the given key is currently up.
-        
+
         Args:
+        ---
             key (string): The given key (w, a, s, d or i, j, k, l)
         """
         pyray_key = self._keys[key.lower()]
@@ -36,8 +37,9 @@ class KeyboardService:
 
     def is_key_down(self, key):
         """Checks if the given key is currently down.
-        
+
         Args:
+        ---
             key (string): The given key (w, a, s, d or i, j, k, l)
         """
         pyray_key = self._keys[key.lower()]
