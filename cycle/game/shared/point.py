b8eafa966e9cd8a71bfd87e98047026d53c113d6
deadshadow99 <97075743+deadshadow99@users.noreply.github.com>
2022-06-16 23:13:12 -0500

    Update point.py

--- cycle/game/shared/point.py
+++ cycle/game/shared/point.py
@@ -1,18 +1,20 @@
 class Point:
     """A distance from a relative origin (0, 0).
 
-    The responsibility of Point is to hold and provide information about itself. Point has a few 
+    The responsibility of Point is to hold and provide information about itself. Point has a few
     convenience methods for adding, scaling, and comparing them.
 
     Attributes:
+    ---
         _x (integer): The horizontal distance from the origin.
         _y (integer): The vertical distance from the origin.
     """
-    
+
     def __init__(self, x, y):
         """Constructs a new Point using the specified x and y values.
-        
+
         Args:
+        ---
             x (int): The specified x value.
             y (int): The specified y value.
         """
@@ -23,6 +25,7 @@ class Point:
         """Gets a new point that is the sum of this and the given one.
 
         Args:
+        ---
             other (Point): The Point to add.
 
         Returns:
@@ -30,31 +33,35 @@ class Point:
         """
         x = self._x + other.get_x()
         y = self._y + other.get_y()
+
         return Point(x, y)
 
     def equals(self, other):
         """Whether or not this Point is equal to the given one.
 
         Args:
+        ---
             other (Point): The Point to compare.
 
-        Returns: 
+        Returns:
             boolean: True if both x and y are equal; false if otherwise.
         """
         return self._x == other.get_x() and self._y == other.get_y()
 
     def get_x(self):
         """Gets the horizontal distance.
-        
+
         Returns:
+        ---
             integer: The horizontal distance.
         """
         return self._x
 
     def get_y(self):
         """Gets the vertical distance.
-        
+
         Returns:
+        ---
             integer: The vertical distance.
         """
         return self._y
@@ -63,10 +70,12 @@ class Point:
         """Reverses the point by inverting both x and y values.
 
         Returns:
+        ---
             Point: A new point that is reversed.
         """
         new_x = self._x * -1
         new_y = self._y * -1
+
         return Point(new_x, new_y)
 
     def scale(self, factor):
@@ -74,9 +83,11 @@ class Point:
         Scales the point by the provided factor.
 
         Args:
+        ---
             factor (int): The amount to scale.
-            
+
         Returns:
+        ---
             Point: A new Point that is scaled.
         """
         return Point(self._x * factor, self._y * factor)
