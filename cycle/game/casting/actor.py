3b84a61f4482612b9db7a423da0866ca7d6f0874
deadshadow99 <97075743+deadshadow99@users.noreply.github.com>
2022-06-16 23:06:04 -0500

    Update actor.py

--- cycle/game/casting/actor.py
+++ cycle/game/casting/actor.py
@@ -4,11 +4,10 @@ from game.shared.point import Point
 
 
 class Actor:
-    """A visible, moveable thing that participates in the game. 
-    
-    The responsibility of Actor is to keep track of its appearance, position and velocity in 2d 
-    space.
+    """A visible, moveable thing that participates in the game.
 
+    The responsibility of Actor is to keep track of its appearance, position and velocity in 2d
+    space.
     Attributes:
         _text (string): The text to display
         _font_size (int): The font size to use.
@@ -27,49 +26,55 @@ class Actor:
 
     def get_color(self):
         """Gets the actor's color as a tuple of three ints (r, g, b).
-        
+
         Returns:
+        ---
             Color: The actor's text color.
         """
         return self._color
 
     def get_font_size(self):
         """Gets the actor's font size.
-        
+
         Returns:
+        ---
             Point: The actor's font size.
         """
         return self._font_size
 
     def get_position(self):
         """Gets the actor's position in 2d space.
-        
+
         Returns:
+        ---
             Point: The actor's position in 2d space.
         """
         return self._position
-    
+
     def get_text(self):
         """Gets the actor's textual representation.
-        
+
         Returns:
+        ---
             string: The actor's textual representation.
         """
         return self._text
 
     def get_velocity(self):
         """Gets the actor's speed and direction.
-        
+
         Returns:
+        ---
             Point: The actor's speed and direction.
         """
         return self._velocity
-    
+
     def move_next(self):
-        """Moves the actor to its next position according to its velocity. Will wrap the position 
+        """Moves the actor to its next position according to its velocity. Will wrap the position
         from one side of the screen to the other when it reaches the given maximum x and y values.
-        
+
         Args:
+        ---
             max_x (int): The maximum x value.
             max_y (int): The maximum y value.
         """
@@ -79,40 +84,45 @@ class Actor:
 
     def set_color(self, color):
         """Updates the color to the given one.
-        
+
         Args:
+        ---
             color (Color): The given color.
         """
         self._color = color
 
     def set_position(self, position):
         """Updates the position to the given one.
-        
+
         Args:
+        ---
             position (Point): The given position.
         """
         self._position = position
-    
+
     def set_font_size(self, font_size):
         """Updates the font size to the given one.
-        
+
         Args:
+        ---
             font_size (int): The given font size.
         """
         self._font_size = font_size
-    
+
     def set_text(self, text):
         """Updates the text to the given value.
-        
+
         Args:
+        ---
             text (string): The given value.
         """
         self._text = text
 
     def set_velocity(self, velocity):
         """Updates the velocity to the given one.
-        
+
         Args:
+        ---
             velocity (Point): The given velocity.
         """
         self._velocity = velocity
