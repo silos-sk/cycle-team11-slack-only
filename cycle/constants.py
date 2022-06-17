38eb8f9ca30a62e71b897a5c00bb6df692519874
deadshadow99 <97075743+deadshadow99@users.noreply.github.com>
2022-06-16 23:13:45 -0500

    Update constants.py

--- cycle/constants.py
+++ cycle/constants.py
@@ -1,6 +1,6 @@
 from game.shared.color import Color
 
-
+# Characteristics per player
 COLUMNS = 40
 ROWS = 20
 CELL_SIZE = 15
@@ -8,9 +8,13 @@ MAX_X = 900
 MAX_Y = 600
 FRAME_RATE = 15
 FONT_SIZE = 15
-CAPTION = "Snake"
-SNAKE_LENGTH = 8
+CAPTION = "Cycle"
+CYCLE_LENGTH = 8
 WHITE = Color(255, 255, 255)
 RED = Color(255, 0, 0)
 YELLOW = Color(255, 255, 0)
 GREEN = Color(0, 255, 0)
+# x component for score
+SCORE_X = 350
+# y component for score
+SCORE_Y = 0
