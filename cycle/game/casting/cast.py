02c7cab6698e943892b25441453cb215ff16f079
deadshadow99 <97075743+deadshadow99@users.noreply.github.com>
2022-06-16 23:05:26 -0500

    Update cast.py

--- cycle/game/casting/cast.py
+++ cycle/game/casting/cast.py
@@ -1,48 +1,53 @@
 class Cast:
     """A collection of actors.
 
-    The responsibility of a cast is to keep track of a collection of actors. It has methods for 
+    The responsibility of a cast is to keep track of a collection of actors. It has methods for
     adding, removing and getting them by a group name.
 
     Attributes:
+    ---
         _actors (dict): A dictionary of actors { key: group_name, value: a list of actors }
     """
 
     def __init__(self):
         """Constructs a new Actor."""
         self._actors = {}
-        
+
     def add_actor(self, group, actor):
         """Adds an actor to the given group.
-        
+
         Args:
+        ---
             group (string): The name of the group.
             actor (Actor): The actor to add.
         """
         if not group in self._actors.keys():
             self._actors[group] = []
-            
+
         if not actor in self._actors[group]:
             self._actors[group].append(actor)
 
     def get_actors(self, group):
         """Gets the actors in the given group.
-        
+
         Args:
+        ---
             group (string): The name of the group.
 
         Returns:
+        ---
             List: The actors in the group.
         """
         results = []
         if group in self._actors.keys():
             results = self._actors[group].copy()
         return results
-    
+
     def get_all_actors(self):
         """Gets all of the actors in the cast.
-        
+
         Returns:
+        ---
             List: All of the actors in the cast.
         """
         results = []
@@ -52,11 +57,13 @@ class Cast:
 
     def get_first_actor(self, group):
         """Gets the first actor in the given group.
-        
+
         Args:
+        ---
             group (string): The name of the group.
-            
+
         Returns:
+        ---
             List: The first actor in the group.
         """
         result = None
@@ -66,8 +73,9 @@ class Cast:
 
     def remove_actor(self, group, actor):
         """Removes an actor from the given group.
-        
+
         Args:
+        ---
             group (string): The name of the group.
             actor (Actor): The actor to remove.
         """
