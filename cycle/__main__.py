8171547a538aa1415edccccb2e8fc406134909c9
deadshadow99 <97075743+deadshadow99@users.noreply.github.com>
2022-06-16 22:59:55 -0500

    Update __main__.py
    Created main

--- cycle/__main__.py
+++ cycle/__main__.py
@@ -1,6 +1,8 @@
 import constants
 
 from game.casting.cast import Cast
+from game.casting.score import Score
+from game.casting.cycle import Cycle
 from game.scripting.script import Script
 from game.scripting.control_actors_action import ControlActorsAction
 from game.scripting.move_actors_action import MoveActorsAction
@@ -12,20 +14,41 @@ from game.services.video_service import VideoService
 from game.shared.color import Color
 from game.shared.point import Point
 
-
 def main():
 
-    # create the cast
-    cast = Cast()
+    # Creates two cycles, with color and positions
+    cycle_one = Cycle(Point(int(constants.MAX_X - 600), int(constants.MAX_Y / 2)))
+    cycle_two = Cycle(Point(int(constants.MAX_X - 300), int(constants.MAX_Y / 2)))
+    cycle_one.set_cycle_color(constants.RED)
+    cycle_two.set_cycle_color(constants.GREEN)
+    cycle_one_name = "Player one"
+    cycle_two_name = "Player two"
+    cycle_one.set_name(cycle_one_name)
+    cycle_two.set_name(cycle_two_name)
 
-    # start the game
+    # Create the cast
+    cast = Cast()
+    score1 = Score()
+    score2 = Score()
+    score1.set_position(Point(constants.MAX_X - 850, 0))
+    score2.set_position(Point(constants.MAX_X - 200, 0))
+    score1.add_points(3)
+    score2.add_points(3)
+    score1.set_player_name(cycle_one_name)
+    score2.set_player_name(cycle_two_name)
+    cast.add_actor("cycle_one", cycle_one)
+    cast.add_actor("cycle_two", cycle_two)
+    cast.add_actor("score1", score1)
+    cast.add_actor("score2", score2)
+
+    # Start the game
     keyboard_service = KeyboardService()
     video_service = VideoService()
 
     script = Script()
     script.add_action("input", ControlActorsAction(keyboard_service))
-    script.add_action("update", MoveActorsAction())
     script.add_action("update", HandleCollisionsAction())
+    script.add_action("update", MoveActorsAction())
     script.add_action("output", DrawActorsAction(video_service))
 
     director = Director(video_service)
