930e604549530371fdf644635a7900e9df9bd94f
deadshadow99 <97075743+deadshadow99@users.noreply.github.com>
2022-06-16 23:12:03 -0500

    Update video_service.py

--- cycle/game/services/video_service.py
+++ cycle/game/services/video_service.py
@@ -3,14 +3,15 @@ import constants
 
 
 class VideoService:
-    """Outputs the game state. The responsibility of the class of objects is to draw the game state 
-    on the screen. 
+    """Outputs the game state. The responsibility of the class of objects is to draw the game state
+    on the screen.
     """
 
     def __init__(self, debug = False):
         """Constructs a new VideoService using the specified debug mode.
-        
+
         Args:
+        ---
             debug (bool): whether or not to draw in debug mode.
         """
         self._debug = debug
@@ -27,13 +28,14 @@ class VideoService:
         pyray.clear_background(pyray.BLACK)
         if self._debug == True:
             self._draw_grid()
-    
+
     def draw_actor(self, actor, centered=False):
         """Draws the given actor's text on the screen.
 
         Args:
+        ---
             actor (Actor): The actor to draw.
-        """ 
+        """
         text = actor.get_text()
         x = actor.get_position().get_x()
         y = actor.get_position().get_y()
@@ -44,28 +46,30 @@ class VideoService:
             width = pyray.measure_text(text, font_size)
             offset = int(width / 2)
             x -= offset
-            
+
         pyray.draw_text(text, x, y, font_size, color)
-        
+
     def draw_actors(self, actors, centered=False):
         """Draws the text for the given list of actors on the screen.
 
         Args:
+        ---
             actors (list): A list of actors to draw.
-        """ 
+        """
         for actor in actors:
             self.draw_actor(actor, centered)
-    
+
     def flush_buffer(self):
         """Copies the buffer contents to the screen. This method should be called at the end of
         the game's output phase.
-        """ 
+        """
         pyray.end_drawing()
 
     def is_window_open(self):
         """Whether or not the window was closed by the user.
 
         Returns:
+        ---
             bool: True if the window is closing; false if otherwise.
         """
         return not pyray.window_should_close()
@@ -74,6 +78,7 @@ class VideoService:
         """Opens a new window with the provided title.
 
         Args:
+        ---
             title (string): The title of the window.
         """
         pyray.init_window(constants.MAX_X, constants.MAX_Y, constants.CAPTION)
@@ -83,10 +88,17 @@ class VideoService:
         """Draws a grid on the screen."""
         for y in range(0, constants.MAX_Y, constants.CELL_SIZE):
             pyray.draw_line(0, y, constants.MAX_X, y, pyray.GRAY)
-            
+
         for x in range(0, constants.MAX_X, constants.CELL_SIZE):
             pyray.draw_line(x, 0, x, constants.MAX_Y, pyray.GRAY)
-    
+
     def _get_x_offset(self, text, font_size):
+        """Gets the offset measurement for the text.
+        
+        Args:
+        ---
+            text (string): The actor's textual representation.
+            font_size (constant): The size of an actor.
+        """
         width = pyray.measure_text(text, font_size)
         return int(width / 2)
